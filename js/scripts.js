4863c492f513eef22177e34da028ec0c0bc8b190
Misaki Koonce <misaki.koonce@gmail.com>
2019-08-30 13:28:14 -0700

    Fix createOrder function - push selected values in array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,70 +1,79 @@
 //Business logic
 // order constructor
-function Order (size, toppings) {
+function Order (size, proteins, veggies, others) {
     this.size = size,
-    this.toppings = toppings,
-    this.proteinToppings = [],
-    this.veggieToppings = [],
-    this.otherToppings = [],
+    this.proteins = proteins;
+    this.veggies = veggies;
+    this.others = others;
     this.price = 0
 }
 
 // Calc base pizza price depends on the pizza size
 Order.prototype.sizePrice = function() {
     if(this.size === "small"){
-        this.price = 8;
+        return 8;
     } else if (this.size === "medium") {
-        this.price = 10;
+        return 10;
     } else {
-        this.price = 11;
+        return 11;
     }
 }
 
-// Push protein toppings
-Order.prototype.proteinPrice = function(protein) {
-    this.proteinToppings.push(protein);
-    this.proteinToppings.forEach(() => {
-        this.cost += 3;
-    })
-}
-
-// Push veggie toppings
-Order.prototype.veggiePrice = function(veggie) {
-    this.veggieToppings.push(veggie);
-    this.veggieToppings.forEach(() => {
-        this.cost += 2;
-    })
-}
-
-// Push other toppings
-Order.prototype.otherPrice = function(other) {
-    this.otherToppings.push(other);
-    this.otherToppings.forEach(() => {
-        this.cost += 1.5;
-    })
+// Update this.price depends on toppings
+Order.prototype.toppingPrice = function() {
+    let cost = 0;
+    console.log('proteins in toppingPrice', this.proteins);
+    this.proteins.forEach(() => {
+        cost += 3;
+    });
+    this.veggies.forEach(() => {
+        cost += 2;
+    });
+    this.others.forEach(() => {
+        cost += 1.5;
+    });
+    console.log("topping total:", cost);
+    return cost;
 }
 
 Order.prototype.totalPrice = function() {
-    this.price = this.sizePrice() + this.proteinPrice() + this.veggiePrice() + this.otherToppings();
-    console.log('order price:', this.price);
+    this.price = this.sizePrice() + this.toppingPrice();
+    console.log("size Price:", this.sizePrice());
+    console.log('topping total price:', this.price);
 }
 
 //User Interface logic
 const createOrder = () => {
+    // size, three toppings]
     const size = $("option:selected").val();
-    const proteinTopping = $("input[name=protein]:checked").val();
-    const veggieTopping = $("input[name=veggies]:checked").val();
-    const otherTopping = $("input[name=others]:checked").val();
-    console.log('size:', size);
-    console.log('protein:', proteinTopping);
-    console.log('veggie:', veggieTopping);
-    console.log('others:', otherTopping);
+    // make arrays of checked items
+    let proteins = [];
+    $(".proteins input[name=proteins]:checked").each(function() {
+        proteins.push($(this).val());
+    });
+    console.log('proteins', proteins);
+    let veggies = [];
+    $(".veggies input[name=veggies]:checked").each(function() {
+        veggies.push($(this).val());
+    });
+    console.log('veggies', veggies);
+    let others = [];
+    $(".others input[name=others]:checked").each(function() {
+        others.push($(this).val());
+    });
+    console.log('others', others);
+    const newOrder = new Order(size, proteins, veggies, others);
+    console.log(newOrder);
+    newOrder.totalPrice();
+    // ToDo: invoke clear form function
+
 }
 
 $(document).ready(function(){
     $("form").submit(function(event){
         event.preventDefault();
         createOrder();
+
     });
 });
 
